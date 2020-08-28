913a59569c89f08a44139b2a950eb44b7e70831b
Nam Vũ <namthp99@gmail.com>
2020-08-28 07:36:41 +0700

    Tiny fix

--- db/schema.rb
+++ db/schema.rb
@@ -35,11 +35,12 @@ ActiveRecord::Schema.define(version: 20200811020726) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "username"
+    t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "room_messages", "rooms"
