b6ba54a0a3075b156c887b15d87b081df2e0b04f
Nam Vũ <namthp99@gmail.com>
2020-08-31 10:22:46 +0700

    react-js

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root controller: :homepage, action: :index
+
   devise_for :users
 
-  root controller: :rooms, action: :index
+  # root controller: :rooms, action: :index
 
   resources :room_messages
   resources :rooms
